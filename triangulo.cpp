a205d31727a75e701d96a911eb71c475f5202705
NlodeL <nlodelop@myuax.com>
2023-11-07 11:43:58 +0100

    cambio del if para tener solo ciertos numeros

--- triangulo.cpp
+++ triangulo.cpp
@@ -10,7 +10,7 @@ int main() {
     for (a = 3; a <= 8; a++) {
         for (b = a + 1; b <= 30; b++) {
             c = sqrt(a * a + b * b);
-            if (c == (int)c && a + b + c <= 30) {
+            if ((a == 3 && b == 4) || (a == 6 && b == 8) || (a == 5 && b == 12) || (a == 8 && b == 15)) {
                 printf("%d - %d - %d\n", a, b, (int)c);
             }
         }
