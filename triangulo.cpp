75474e553ab095532d1ebe07c7f460a5e66e1e3d
NlodeL <nlodelop@myuax.com>
2023-11-07 11:24:26 +0100

    if para lado 3

--- triangulo.cpp
+++ triangulo.cpp
@@ -4,6 +4,10 @@
 int (a, b, c);
 for (a = 1; a < 30; a++) {
     for (b = a + 1; b < 30; b++) {
+        c = sqrt(a * a + b * b);
+        if (c == (int)c && a + b + c <= 30) {
+            printf("%d - %d - %d\n", a, b, (int)c);
+        }
     }
 }
 
