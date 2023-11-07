e6f3e24fe63531df09899c263d565d5c89a875d8
NlodeL <nlodelop@myuax.com>
2023-11-07 11:29:06 +0100

    arreglo código

--- triangulo.cpp
+++ triangulo.cpp
@@ -3,19 +3,18 @@
 //
 #include <stdio.h>
 #include <math.h>
-#include <iostream>
 
 int main() {
-int (a, b, c);
+    int a, b, c;
 
-for (a = 1; a < 30; a++) {
-    for (b = a + 1; b < 30; b++) {
-        c = sqrt(a * a + b * b);
-        if (c == (int)c && a + b + c <= 30) {
-            printf("%d - %d - %d\n", a, b, (int)c);
+    for (a = 1; a <= 30; a++) {
+        for (b = a + 1; b <= 30; b++) {
+            c = sqrt(a * a + b * b);
+            if (c == (int)c && a + b + c <= 30) {
+                printf("%d - %d - %d\n", a, b, (int)c);
+            }
         }
     }
-}
 
-return 0;
-    }
+    return 0;
+}
